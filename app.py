6a51bbf004f40d6ec890dd9501d81a74e0fb6246
sam <wax654@gmail.com>
2021-03-11 12:53:10 -0800

    added User class

---
+++ app.py
@@ -0,0 +1,18 @@
+"""Blogly application."""
+
+from flask import Flask
+from models import db, connect_db, User
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'a secret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
+db.create_all()
+
+u = User(first_name="jake", last_name="paulson", image_url="https://www.homepont.com/ehns/pic")
+
+db.session.add(u)
+db.session.commit()
---
+++ models.py
@@ -0,0 +1,17 @@
+"""Models for Blogly."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+    
+class User(db.Model):
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key=True,autoincrement=True)
+    first_name  = db.Column(db.TEXT, nullable = False)
+    last_name = db.Column(db.TEXT, nullable=True)
+    image_url = db.Column(db.TEXT, nullable=True)
+    
