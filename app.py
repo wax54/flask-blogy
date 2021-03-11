774a93734ecbe16d691cb8ac5a46e03050dab1ce
sam <wax654@gmail.com>
2021-03-11 14:01:05 -0800

    making my way downtown

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 """Blogly application."""
 
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 from models import db, connect_db, User
 
 app = Flask(__name__)
@@ -12,7 +12,44 @@ app.config['SQLALCHEMY_ECHO'] = True
 connect_db(app)
 db.create_all()
 
-u = User(first_name="jake", last_name="paulson", image_url="https://www.homepont.com/ehns/pic")
+# u = User(first_name="jake", last_name="paulson", image_url="https://www.homepont.com/ehns/pic")
 
-db.session.add(u)
-db.session.commit()
+# db.session.add(u)
+# db.session.commit()
+
+
+@app.route('/')
+def home_page():
+    return redirect('/users')
+
+
+@app.route('/users')
+def list_of_users():
+    users = User.query.all()
+    return render_template('user_list.html', users=users)
+
+
+@app.route('/users/new')
+def new_user_form():
+    return render_template('new_user.html')
+
+
+@app.route('/users/new', methods=["POST"])
+def submit_new_user():
+    f_name = request.form['first_name']
+    l_name = request.form['last_name']
+    image = request.form['image_url']
+    
+    new_user = User(first_name=f_name, last_name=l_name, image_url=image)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect('/')
+
+
+@app.route('users/<int:user_id>')
+def describe_user(user_id):
+    user = User.query.get(user_id)
+    return render_template('user_description.html', user=user)
+
+
+
